908f70b21fd00b17040a582dbe3cfff48f1f05ae
Pawan Kumar <pawan41281@gmail.com>
2025-07-18 23:55:49 +0530

    Add files via upload

---
+++ src/main/java/com/example/kafkaproducerapi/KafkaproducerapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaproducerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaproducerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaproducerapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkaproducerapi/KafkaproducerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaproducerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaproducerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
